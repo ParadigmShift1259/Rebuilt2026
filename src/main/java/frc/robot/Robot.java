0ffd3c9a1d05fdfe11d6ca4e3733c79ec8d4d134
Scott Coursin <scott.coursin@gmail.com>
2026-01-14 19:42:25 -0600

    auto align and questnav code

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,135 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.ctre.phoenix6.HootAutoReplay;
+
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+import gg.questnav.questnav.QuestNav;
+import gg.questnav.questnav.PoseFrame;
+
+public class Robot extends TimedRobot {
+    private Command m_autonomousCommand;
+
+    private final RobotContainer m_robotContainer;
+
+    private QuestNav questNav = new QuestNav();
+
+    Field2d m_field = new Field2d();
+    Matrix<N3, N1> QUESTNAV_STD_DEVS =
+        VecBuilder.fill(
+            0.02, // Trust down to 2cm in X direction
+            0.02, // Trust down to 2cm in Y direction
+            0.035 // Trust down to 2 degrees rotational
+        );
+
+    /* log and replay timestamp and joystick data */
+    private final HootAutoReplay m_timeAndJoystickReplay = new HootAutoReplay()
+        .withTimestampReplay()
+        .withJoystickReplay();
+
+    public Robot() {
+        m_robotContainer = new RobotContainer();
+    }
+
+    @Override
+    public void robotPeriodic() {
+        m_timeAndJoystickReplay.update();
+        CommandScheduler.getInstance().run(); 
+
+        
+    questNav.commandPeriodic();
+
+    if (questNav.isTracking()) {
+        // Get the latest pose data frames from the Quest
+        PoseFrame[] questFrames = questNav.getAllUnreadPoseFrames();
+
+        // Loop over the pose data frames and send them to the pose estimator
+        for (PoseFrame questFrame : questFrames) {
+            // Get the pose of the Quest
+            Pose3d questPose = questFrame.questPose3d();
+            // Get timestamp for when the data was sent
+            double timestamp = questFrame.dataTimestamp();
+
+            // Transform by the mount pose to get your robot pose
+            Pose3d robotPose = questPose;
+
+            // You can put some sort of filtering here if you would like!
+
+            // Add the measurement to our estimator
+            m_robotContainer.drivetrain.addVisionMeasurement(robotPose.toPose2d(), timestamp, QUESTNAV_STD_DEVS);
+
+            SmartDashboard.putNumber("QuestPoseX", questPose.getX());
+            SmartDashboard.putNumber("QuestPoseY", questPose.getY());
+
+            m_field.setRobotPose(questPose.toPose2d());
+
+        }
+    }
+    }
+
+    @Override
+    public void disabledInit() {}
+
+    @Override
+    public void disabledPeriodic() {}
+
+    @Override
+    public void disabledExit() {}
+
+    @Override
+    public void autonomousInit() {
+        m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+        if (m_autonomousCommand != null) {
+            CommandScheduler.getInstance().schedule(m_autonomousCommand);
+        }
+    }
+
+    @Override
+    public void autonomousPeriodic() {}
+
+    @Override
+    public void autonomousExit() {}
+
+    @Override
+    public void teleopInit() {
+        if (m_autonomousCommand != null) {
+            CommandScheduler.getInstance().cancel(m_autonomousCommand);
+        }
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        m_robotContainer.periodic();
+    }
+
+    @Override
+    public void teleopExit() {}
+
+    @Override
+    public void testInit() {
+        CommandScheduler.getInstance().cancelAll();
+    }
+
+    @Override
+    public void testPeriodic() {}
+
+    @Override
+    public void testExit() {}
+
+    @Override
+    public void simulationPeriodic() {}
+}
---
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -0,0 +1,93 @@
+package frc.robot.commands;
+
+import java.util.function.Supplier;
+
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.ctre.phoenix6.swerve.SwerveRequest.ForwardPerspectiveValue;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+
+import com.ctre.phoenix6.swerve.SwerveRequest.ApplyRobotSpeeds;
+// import com.ctre.phoenix6.swerve.SwerveRequest.ApplyChassisSpeeds;
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+
+public class DriveCommands {
+    public static Command driveToPoseCommand(CommandSwerveDrivetrain drive, Pose2d targetPose) {
+
+    final double TRANSLATION_KP = 2.25;
+    final double TRANSLATION_KD = 0.0;
+
+    final double ANGLE_KP = 5.0;
+    final double ANGLE_KD = 0.4;
+
+    final double ANGLE_MAX_VELOCITY = 8.0;
+
+    final double ANGLE_MAX_ACCELERATION = 20.0;
+
+    final SwerveRequest.FieldCentricFacingAngle m_request = new SwerveRequest.FieldCentricFacingAngle()
+    .withForwardPerspective(ForwardPerspectiveValue.BlueAlliance)
+    .withDriveRequestType(DriveRequestType.Velocity)
+    .withHeadingPID(4, 0, 0); /* change these values for your robot */
+
+
+    PIDController xController = new PIDController(TRANSLATION_KP, 0, TRANSLATION_KD);
+    PIDController yController = new PIDController(TRANSLATION_KP, 0, TRANSLATION_KD);
+    ProfiledPIDController angleController =
+        new ProfiledPIDController(
+            ANGLE_KP,
+            0.0,
+            ANGLE_KD,
+            new TrapezoidProfile.Constraints(ANGLE_MAX_VELOCITY, ANGLE_MAX_ACCELERATION));
+    angleController.enableContinuousInput(-Math.PI, Math.PI);
+
+    return Commands.run(
+            () -> {
+              Pose2d currentPose = drive.getState().Pose;
+              // Calculate angular speed
+              double omega =
+                  angleController.calculate(
+                      currentPose.getRotation().getRadians(),
+                      targetPose.getRotation().getRadians());
+
+              // Convert to field relative speeds & send command
+
+              double xOutput = xController.calculate(currentPose.getX(), targetPose.getX());
+              double yOutput = yController.calculate(currentPose.getY(), targetPose.getY());
+
+              if (Math.abs(xOutput) < 0.05) {
+                xOutput = 0;
+              }
+
+              if (Math.abs(yOutput) < 0.05) {
+                yOutput = 0;
+              }
+
+              ChassisSpeeds speeds = new ChassisSpeeds(xOutput, yOutput, omega);
+              boolean isFlipped =
+                  DriverStation.getAlliance().isPresent()
+                      && DriverStation.getAlliance().get() == Alliance.Red;
+                drive.setControl(m_request.withVelocityX(speeds.vxMetersPerSecond).withVelocityY(speeds.vyMetersPerSecond).withTargetDirection(targetPose.getRotation()));
+
+            },
+            drive)
+        .until(
+            () -> targetPose.getTranslation().getDistance(drive.getState().Pose.getTranslation()) < 0.05)
+
+        // Reset PID controller when command starts
+        .beforeStarting(
+            () -> {
+              angleController.reset(drive.getState().Pose.getRotation().getRadians());
+              xController.setSetpoint(targetPose.getX());
+              yController.setSetpoint(targetPose.getY());
+            });
+  }
+}
