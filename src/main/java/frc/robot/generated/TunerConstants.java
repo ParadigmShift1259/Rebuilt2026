b10515b50a68cbfe427c1ce56407baaa6bb075b0
Scott Coursin <scott.coursin@gmail.com>
2026-02-07 16:39:38 -0600

    Set PID P=100 for test sled

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -24,7 +24,7 @@ public class TunerConstants {
     // The steer motor uses any SwerveModule.SteerRequestType control request with the
     // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
     private static final Slot0Configs steerGains = new Slot0Configs()
-        .withKP(10).withKI(0).withKD(0.0)
+        .withKP(100).withKI(0).withKD(0.0)
         .withKS(0.0).withKV(2.39).withKA(0)
         .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
     // When using closed-loop control, the drive motor uses the control
