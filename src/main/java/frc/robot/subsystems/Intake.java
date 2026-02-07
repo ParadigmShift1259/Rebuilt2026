32882a15f9428e792959566750f46d45f379f470
Scott Coursin <scott.coursin@gmail.com>
2026-02-07 16:38:04 -0600

    Use Votex for intake roller

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -21,6 +21,7 @@ import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 // import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -28,10 +29,12 @@ public class Intake extends SubsystemBase {
     private SparkMax m_deployMotor = new SparkMax(21, SparkMax.MotorType.kBrushless);
     private RelativeEncoder m_deployEnc = m_deployMotor.getEncoder();
     private SparkClosedLoopController m_deployClc = m_deployMotor.getClosedLoopController();
-    private final TalonFX m_rollerMotor = new TalonFX(11);
-    private final MotionMagicVoltage m_mmReq = new MotionMagicVoltage(0);
+    //private final TalonFX m_rollerMotor = new TalonFX(4);
+    private SparkMax m_rollerMotor = new SparkMax(4, SparkMax.MotorType.kBrushless);
 
     public Intake() {
+        SmartDashboard.putNumber("intakeVoltage", 6);
+
         SparkMaxConfig config = new SparkMaxConfig();
         config.idleMode(SparkMaxConfig.IdleMode.kBrake)
             .inverted(false)
@@ -40,28 +43,11 @@ public class Intake extends SubsystemBase {
         m_deployMotor.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
         m_deployEnc.setPosition(0.0);
 
-        TalonFXConfiguration cfg = new TalonFXConfiguration();
-        FeedbackConfigs fdb = cfg.Feedback;
-        fdb.SensorToMechanismRatio = 1; // TODO figure out gear ratio
-        
-        MotionMagicConfigs mm = cfg.MotionMagic;
-        mm.withMotionMagicCruiseVelocity(RotationsPerSecond.of(5))
-          .withMotionMagicAcceleration(RotationsPerSecondPerSecond.of(10))
-          .withMotionMagicJerk(RotationsPerSecondPerSecond.per(Second).of(100));
-        
-        Slot0Configs slot0 = cfg.Slot0;
-        slot0.kP = 60;
-        slot0.kI = 0;
-        slot0.kD = 0.5;
-
-        StatusCode status = StatusCode.StatusCodeNotInitialized;
-        for (int i = 0; i < 5; ++i) {
-            status = m_rollerMotor.getConfigurator().apply(cfg);
-            if (status.isOK()) break;
-        }
-        if (!status.isOK()) {
-            System.out.println("Could not configure device. Error: " + status.toString());
-        }
+        config.idleMode(SparkMaxConfig.IdleMode.kCoast)
+            .inverted(false)
+            .closedLoopRampRate(0.0)
+            .closedLoop.outputRange(-1.0,1.0, ClosedLoopSlot.kSlot0);
+        m_rollerMotor.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
     }
 
     public void deploy() {
@@ -73,12 +59,14 @@ public class Intake extends SubsystemBase {
     }
 
     public void runIntake() {
-        m_rollerMotor.setControl(m_mmReq.withPosition(10).withSlot(0));
-        m_rollerMotor.setPosition(Rotations.of(1)); // TODO figure out rot/sec
+        double volt = SmartDashboard.getNumber("intakeVoltage", 6);
+        m_rollerMotor.setVoltage(volt);
+        // m_rollerMotor.setPosition(Rotations.of(1)); // TODO figure out rot/sec
     }
     
     public void stopIntake() {
-        m_rollerMotor.setControl(m_mmReq.withPosition(0).withSlot(0));
-        m_rollerMotor.setPosition(Rotations.of(0));
+        m_rollerMotor.setVoltage(0);
+        // m_rollerMotor.setControl(m_mmReq.withPosition(0).withSlot(0));
+        // m_rollerMotor.setPosition(Rotations.of(0));
     }
 }
