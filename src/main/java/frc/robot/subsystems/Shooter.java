4e9436902de59cde2f8bbb01852aba0c7c38ca99
Scott Coursin <scott.coursin@gmail.com>
2026-03-20 20:34:20 -0500

    Removed flywheel toggle check

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -232,12 +232,12 @@ public class Shooter extends SubsystemBase {
         SmartDashboard.putNumber("TurretDegCalc", m_turretAngle * 180.0 / Math.PI);
         
         boolean isTesting = SmartDashboard.getBoolean("disableShooter", Constants.defaultFlywheel); // for reducing noise during testing
-        if (!isTesting) {
-            setRPMDistanceAndVelo(m_ChassisSpeeds);
-        }
-        else {
-            stopShooter();
-        }
+        // if (!isTesting) {
+        setRPMDistanceAndVelo(m_ChassisSpeeds);
+        // }
+        // else {
+        //     stopShooter();
+        // }
     }
     public void resetPrevDist() { m_prevDistance = 0.0; }
 
